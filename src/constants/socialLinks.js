559537d4e4a473e9c461ec3935f35db9a3e6c90d
Craig Booker <817578+craigbooker@users.noreply.github.com>
2021-06-28 14:11:37 -0500

    Base project files.

--- src/constants/socialLinks.js
+++ src/constants/socialLinks.js
@@ -1,24 +1,24 @@
-import React from "react"
+import React from 'react'
 import {
   FaFacebookSquare,
   FaDribbbleSquare,
   FaTwitterSquare,
-} from "react-icons/fa"
+} from 'react-icons/fa'
 const SocialLinks = ({ styleClass }) => {
   return (
     <ul className={styleClass}>
       <li>
-        <a href="https://twitter.com">
+        <a href="https://facebook.com/craigbooker">
           <FaFacebookSquare className="social-icon facebook-icon"></FaFacebookSquare>
         </a>
       </li>
       <li>
-        <a href="https://twitter.com">
+        <a href="https://dribbble.com/craigbooker">
           <FaDribbbleSquare className="social-icon dribble-icon"></FaDribbbleSquare>
         </a>
       </li>
       <li>
-        <a href="https://twitter.com">
+        <a href="https://twitter.com/craigbooker">
           <FaTwitterSquare className="social-icon twitter-icon"></FaTwitterSquare>
         </a>
       </li>
