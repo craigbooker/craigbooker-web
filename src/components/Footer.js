96320c4123139fdde016033936099348f78954cb
Craig Booker <817578+craigbooker@users.noreply.github.com>
2021-08-19 12:35:37 -0500

    Changed footer to dynamically insert copyright.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import SocialLinks from '../constants/socialLinks'
+import SiteConfig from '../../data/SiteConfig'
+
 const Footer = () => {
   return (
     <footer className="footer">
       <div>
         <SocialLinks styleClass="footer-icons" />
-        <p>&copy;{new Date().getFullYear()} MDXBlog. All rights reserved.</p>
+        <p>
+          &copy;{new Date().getFullYear()} {SiteConfig.copyright}. All rights
+          reserved.
+        </p>
       </div>
     </footer>
   )
