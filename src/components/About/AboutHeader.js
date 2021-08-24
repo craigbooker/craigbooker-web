1ff0e5bf45b5cdb89dd1f2da5978bb9f137e5bb5
Craig Booker <817578+craigbooker@users.noreply.github.com>
2021-08-24 10:32:20 -0500

    Made style changes to the aboutHeader which appears on the about page.

--- src/components/About/AboutHeader.js
+++ src/components/About/AboutHeader.js
@@ -31,39 +31,24 @@ const AboutHeader = () => {
       <Title title="about" subtitle="craig" />
       <div className={aboutCenter}>
         <article className={aboutImg}>
-          <div className={imgContainer}>
-            {/* <img src={img} alt="about company" /> */}
-            <StaticImage
-              src="../../assets/images/craigbookerSquare.jpg"
-              alt="Craig Booker photo"
-            />
-            {/* <Img
-              fluid={aboutImage.childImageSharp.fluid}
-              alt="awesome landscape"
-            /> */}
-          </div>
+          <StaticImage
+            src="../../assets/images/craigbookerOval.jpg"
+            alt="Craig Booker photo"
+          />
         </article>
         <article className={aboutInfo}>
           <h4></h4>
           <p>
             I'm a Writer and Software Developer from Oklahoma City, Oklahoma. My
-            love for electronics started while tinkering with electronics when I
-            was young. This love for electronic turned into a love for computers
-            which in turn lead me to software. My earliest memeories of writing
-            was between 2000 and 2001. I wrote off and on for fifteen years
-            before I would discover it was a gift. It was always just something
-            I did in my spare time to express myself. I never considered it as
-            my passion or as a potential career.
-          </p>
-          <p>
-            It was while working at Apple where I would learn about Apple’s
-            methods in training individuals to get the most out of their
-            technology. I also learned about their people-first culture and a
-            true appreciation of people of all types. It was while working at
-            Apple that I affirmed some of my ideas on customer service. I aim to
-            take what I learned at Apple, combining the best customer service, a
-            love for great apps and dedication to customers to provide the best
-            products and services.
+            childhood love for electronics lead me to software. I began writing
+            in 2000 and I wrote off and on for fifteen years before I would
+            discover it was a gift. It was always just something I did in my
+            spare time to express myself. I never considered it as my passion or
+            as a potential career. While working at Apple I learned a great deal
+            about problem solving and customer service. I aim to take what I
+            learned at Apple, combining the best customer service, a love for
+            great apps and dedication to customers to provide the best products
+            and services.
           </p>
         </article>
       </div>
