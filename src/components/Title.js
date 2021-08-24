5faf04afa5d697a8819cbd1160a9cfe0060e70c8
Craig Booker <817578+craigbooker@users.noreply.github.com>
2021-08-24 07:52:18 -0500

    Fixed the title to show as uppercase.

--- src/components/Title.js
+++ src/components/Title.js
@@ -16,7 +16,9 @@ export default styled(Title)`
   text-transform: uppercase;
   font-size: 2.3rem;
   margin-bottom: 2rem;
+
   h4 {
+    text-transform: uppercase;
     text-align: center;
     letter-spacing: 7px;
     color: var(--primaryColor);
